993c2e1af78c1ffc73374b216cdeeebe85d1192b
SpiteC <pyryrauhala@kamk.fi>
2015-09-21 17:35:44 +0300

    Loput matskut

--- Kerta02/main.cpp
+++ Kerta02/main.cpp
@@ -1,6 +1,61 @@
 #include "Person.h"
 #include "Player.h"
+#include "Car.h"
+#include <iostream>
+
+/*
+Car* carFunc() {
+	Car car("Audi", 55);
+	return &car;
+}*/
 
 int main(int argc, char** argv) {
+
+	/*
+	Person personA("BibleThump", 50);
+	Person personB("Kappa", 30);
+	Person personC("KappaHD", 40);
+
+	std::cout << personA.getName() << std::endl;
+
+	Vector2 v1;
+	Vector2 v2(10.0f, 10.0f);
+
+	Player player1;
+	player1.setPosition(Vector2(256.0f, 12.24f));
+	player1.print();
+
+	{
+		{
+			Car nissan("Nissan", 1232);
+		}
+		Car volkkari("Volkswagen", 200);
+		volkkari.print();
+
+		for (size_t i = 0; i < 5; i++)
+		{
+			volkkari.accelerate();
+		}
+
+		std::cout << std::endl;
+
+		volkkari.print();
+
+		for (size_t i = 0; i < 3; i++)
+		{
+			volkkari.brake();
+		}
+
+		volkkari.print();
+	}
+	*/
+	
+	/*
+	Car* car = carFunc();
+	car->print();
+	*/
+
+
+	std::cin.get();
 	return 0;
 }
